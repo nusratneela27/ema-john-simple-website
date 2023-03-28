761d46af5785f7eefd9143216ae08771dc98eb80
neela <nusratneela27@gmail.com>
2023-03-29 00:57:20 +0600

    new repo

---
+++ src/components/Shop/Shop.jsx
@@ -0,0 +1,61 @@
+import React, { useEffect, useState } from 'react';
+import { addToDb, getShoppingCart } from '../../utilities/fakedb';
+import Cart from '../Cart/Cart';
+import Product from '../Product/Product';
+import './Shop.css';
+
+const Shop = () => {
+    const [products, setProducts] = useState([])
+    const [cart, setCart] = useState([])
+
+    useEffect(() => {
+        fetch('products.json')
+            .then(res => res.json())
+            .then(data => setProducts(data))
+    }, []);
+
+    useEffect(() => {
+        const storedCart = getShoppingCart();
+        const savedCart = [];
+        // step - 1: get id
+        for (const id in storedCart) {
+            // step- 2: get quantity of the product by using id
+            const addedProduct = products.find(product => product.id === id);
+            if (addedProduct) {
+                // step- 3: get quantity of the product
+                const quantity = storedCart[id];
+                addedProduct.quantity = quantity
+                // step- 4: add the added product to the saved cart
+                savedCart.push(addedProduct)
+
+            }
+        }
+        // step-5: save the cart
+        setCart(savedCart)
+    }, [products])
+
+    const handleAddCart = (product) => {
+        const newCart = [...cart, product];
+        setCart(newCart);
+        addToDb(product.id)
+    }
+
+    return (
+        <div className='shop-container'>
+            <div className="products-container">
+                {
+                    products.map(product => <Product
+                        key={product.id}
+                        product={product}
+                        handleAddCart={handleAddCart}
+                    ></Product>)
+                }
+            </div>
+            <div className="cart-container">
+                <Cart cart={cart}></Cart>
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
