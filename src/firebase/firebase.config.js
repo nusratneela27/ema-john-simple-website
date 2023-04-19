9a8c72772a13b177af84dba4bcd64862f711be57
neela <nusratneela27@gmail.com>
2023-04-19 15:13:08 +0600

    AuthProvider added

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBMIiph3_MABhz7D2eeoHxtnOY6NoSvbWM",
+    authDomain: "ema-john-with-firebase-a-913a7.firebaseapp.com",
+    projectId: "ema-john-with-firebase-a-913a7",
+    storageBucket: "ema-john-with-firebase-a-913a7.appspot.com",
+    messagingSenderId: "522432436191",
+    appId: "1:522432436191:web:c5340715fc476156e5166a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
--- src/main.jsx
+++ src/main.jsx
@@ -14,6 +14,8 @@ import Inventory from './components/Inventory/Inventory';
 import Login from './components/Login/Login';
 import cartProductsLoader from './loaders/cartProductsLoader';
 import CheckOut from './components/CheckOut/CheckOut';
+import SignUp from './components/SignUp/SignUp';
+import AuthProvider from './components/Providers/AuthProvider';
 
 const router = createBrowserRouter([
   {
@@ -41,6 +43,10 @@ const router = createBrowserRouter([
       {
         path: 'login',
         element: <Login></Login>
+      },
+      {
+        path: 'signUp',
+        element: <SignUp></SignUp>
       }
     ]
   }
@@ -48,6 +54,8 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </React.StrictMode>,
 )
